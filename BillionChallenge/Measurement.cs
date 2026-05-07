cf264e99f933e9a68b305492263b497171423132
kanevskiy <vremenno.navernoe@gmail.com>
2026-05-07 19:45:22 +0200

    Combine parsing with calculations, avoid creating objects for calculating with LINQ

--- BillionChallenge/Measurement.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BillionChallenge;
-
-public readonly struct Measurement(string location, double temperature)
-{
-    public readonly string Location = location;
-    public readonly double Temperature = temperature;
-}
